ee690b65ce7613818ae98bcf8b0b429134ff7060
enveruntuc <20200808047@ogr.akdeniz.edu.tr>
2025-07-07 16:25:58 +0300

    first commit

---
+++ src/main/java/com/santsg/bookingwebsite/BookingwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.santsg.bookingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santsg/bookingwebsite/BookingwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santsg.bookingwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
