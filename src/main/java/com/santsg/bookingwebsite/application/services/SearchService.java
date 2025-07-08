e4a729edabf9f9787549b480b9cb0f6d66337f5c
enveruntuc <20200808047@ogr.akdeniz.edu.tr>
2025-07-08 13:56:16 +0300

    hotfix

--- src/main/java/com/santsg/bookingwebsite/application/services/SearchService.java
+++ src/main/java/com/santsg/bookingwebsite/application/services/SearchService.java
@@ -1,14 +1,15 @@
 package com.santsg.bookingwebsite.application.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.santsg.bookingwebsite.entities.models.search.ArrivalAutoCompleteRequest;
 import com.santsg.bookingwebsite.entities.models.search.ArrivalAutocompleteResponseBody;
+import com.santsg.bookingwebsite.entities.shared.ApiResponse;
 import com.santsg.bookingwebsite.entities.shared.Globals;
-import org.springframework.http.HttpEntity;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
-
 
 public class SearchService {
     private final RestClient restClient;
@@ -16,8 +17,9 @@ public class SearchService {
     public SearchService() {
         this.restClient = RestClient.create();
     }
-    
-    public ApiResponse<ArrivalAutocompleteResponseBody> getArrivalAutocomplete(String token, int productType, String query, String culture) {
+
+    public ApiResponse<ArrivalAutocompleteResponseBody> getArrivalAutocomplete(String token, int productType,
+            String query, String culture) {
         String apiUrl = Globals.API_URL + "/productservice/getarrivalautocomplete";
         ArrivalAutoCompleteRequest autoCompleteRequest = new ArrivalAutoCompleteRequest();
         autoCompleteRequest.setProductType(productType);
@@ -26,19 +28,22 @@ public class SearchService {
 
         try {
             String response = restClient.post()
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .body(autoCompleteRequest)
-                .retrieve()
-                .body(String.class);
+                    .uri(apiUrl)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                    .body(autoCompleteRequest)
+                    .retrieve()
+                    .body(String.class);
 
             ObjectMapper objectMapper = new ObjectMapper();
-            
-            return objectMapper.readValue(
-                response,
-                objectMapper.getTypeFactory().constructParametricType(ApiResponse.class, ArrivalAutocompleteResponseBody.class)
-            );
+            ApiResponse<ArrivalAutocompleteResponseBody> apiResponse = objectMapper.readValue(
+                    response,
+                    objectMapper.getTypeFactory().constructParametricType(ApiResponse.class,
+                            ArrivalAutocompleteResponseBody.class));
+            return apiResponse;
+        } catch (RestClientException | com.fasterxml.jackson.core.JsonProcessingException e) {
+            return null;
+        }
+
     }
-
 }
-
