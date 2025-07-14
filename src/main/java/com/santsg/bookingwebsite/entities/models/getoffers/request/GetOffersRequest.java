83b21abf3eabdf5f207effe6ad022af9b783f905
Sude Türkdoğan <turkdogansude@gmail.com>
2025-07-14 11:54:00 +0300

    getoffer request model created

--- src/main/java/com/santsg/bookingwebsite/entities/models/getoffers/request/GetOffersRequest.java
+++ src/main/java/com/santsg/bookingwebsite/entities/models/getoffers/request/GetOffersRequest.java
@@ -1,5 +1,11 @@
 package com.santsg.bookingwebsite.entities.models.getoffers.request;
 
 public class GetOffersRequest {
-
+    private String searchId;
+    private String offerId;
+    private int productType;
+    private String productId; // Bu PriceSearchResponse içindeki hotel.id olacak
+    private String currency;
+    private String culture;
+    private boolean getRoomInfo;
 }
