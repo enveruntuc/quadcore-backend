b6232385d32b97203245063614388c39da874408
enveruntuc <20200808047@ogr.akdeniz.edu.tr>
2025-07-14 14:45:58 +0300

    cleanup

--- src/main/java/com/santsg/bookingwebsite/entities/models/getoffers/response/GetOffersResponseBody.java
+++ src/main/java/com/santsg/bookingwebsite/entities/models/getoffers/response/GetOffersResponseBody.java
@@ -1,5 +1,13 @@
 package com.santsg.bookingwebsite.entities.models.getoffers.response;
 
+import java.util.List;
+import lombok.Data;
+
+@Data
 public class GetOffersResponseBody {
+
+    private List<Offers> offers;
+    private Information information;
+    private int productId;
 
 }
