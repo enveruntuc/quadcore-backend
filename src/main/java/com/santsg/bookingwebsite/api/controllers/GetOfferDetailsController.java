93b494dcfff71f16d6a51de678b7b5ddfb7f95c1
Sude Türkdoğan <turkdogansude@gmail.com>
2025-07-18 17:44:39 +0300

    get offer details service and controller created

---
+++ src/main/java/com/santsg/bookingwebsite/api/controllers/GetOfferDetailsController.java
@@ -0,0 +1,35 @@
+package com.santsg.bookingwebsite.api.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.santsg.bookingwebsite.application.services.GetOfferDetailsService;
+import com.santsg.bookingwebsite.entities.models.getofferdetails.request.GetOffersDetailsRequest;
+import com.santsg.bookingwebsite.entities.models.getofferdetails.response.OfferDetailsResponseBody;
+import com.santsg.bookingwebsite.entities.shared.ApiResponse;
+import org.springframework.http.HttpHeaders;
+
+@RestController
+@RequestMapping("/api/getofferdetails")
+public class GetOfferDetailsController {
+
+    private final GetOfferDetailsService getOfferDetailsService;
+    public GetOfferDetailsController(GetOfferDetailsService getOfferDetailsService) {
+        this.getOfferDetailsService = getOfferDetailsService;
+    }
+
+    @PostMapping
+    public ApiResponse<OfferDetailsResponseBody> getOfferDetails(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String token,
+            @RequestBody GetOffersDetailsRequest request) {
+
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+        return getOfferDetailsService.getOfferDetails(token, request);
+    }
+    
+}
---
+++ src/main/java/com/santsg/bookingwebsite/application/services/GetOfferDetailsService.java
@@ -0,0 +1,49 @@
+package com.santsg.bookingwebsite.application.services;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.santsg.bookingwebsite.entities.models.getofferdetails.request.GetOffersDetailsRequest;
+import com.santsg.bookingwebsite.entities.models.getofferdetails.response.OfferDetailsResponseBody;
+import com.santsg.bookingwebsite.entities.shared.ApiResponse;
+import com.santsg.bookingwebsite.entities.shared.Globals;
+
+@Service
+public class GetOfferDetailsService {
+
+    private final RestClient restClient;
+
+    public GetOfferDetailsService() {
+        this.restClient = RestClient.create();
+    }
+
+    public ApiResponse<OfferDetailsResponseBody> getOfferDetails(String token, GetOffersDetailsRequest request) {
+        String apiUrl = Globals.API_URL + "/productservice/getofferdetails";
+        try {
+            String response = restClient.post()
+                    .uri(apiUrl)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                    .body(request)
+                    .retrieve()
+                    .body(String.class);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            ApiResponse<OfferDetailsResponseBody> apiResponse = objectMapper.readValue(
+                    response,
+                    objectMapper.getTypeFactory().constructParametricType(ApiResponse.class, OfferDetailsResponseBody.class)
+            );
+            return apiResponse;
+        } catch (RestClientException | com.fasterxml.jackson.core.JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    
+}
